6070562cf6e862eebda9e894806c90494051425d
naxlin <naxlin.everthaine@gmail.com>
2020-10-08 21:09:27 -0600

    Implemented start button and addtion random nums.

--- CIT365-MathQuiz/Form1.cs
+++ CIT365-MathQuiz/Form1.cs
@@ -14,6 +14,8 @@ namespace CIT365_MathQuiz
     {
         // Creating random number generator:
         Random randomizer = new Random();
+        // Addition random numbers:
+        int addend1, addend2;
 
         public Form1()
         {
@@ -32,6 +34,28 @@ namespace CIT365_MathQuiz
 
         private void quotient_ValueChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
+
+        // Will start the game when the start quiz button is pressed
+        // this includes the creation of random numbers and the timer
+        public void StartTheQuiz()
+        {
+            // Addition problem:
+            // Randomizing numbers:
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+            // Updating the labels accordingly:
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+            // Setting Sum default:
+            sum.Value = 0;
 
         }
     }
